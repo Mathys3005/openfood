84aca4021dce2352f1ac6ae91cd91115b80d3bd5
Mathys <mathys.ledanois@viacesi.fr>
2026-02-18 22:59:49 +0100

    Récupération des produits avec debounce + searchBar

--- app/(main)/add/index.tsx
+++ app/(main)/add/index.tsx
@@ -1,12 +1,47 @@
-import { View, Text, Pressable } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, Pressable, TextInput } from 'react-native'
 import { StyleSheet } from 'react-native'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { mealTypes } from '../../../types/mealType.type'
+import { Ionicons } from '@expo/vector-icons'
+import { Food } from '../../../types/food.type'
+import useDebounce from '../../../tools/debounce'
 
 const AddFoodScreen = () => {
     const [selectedMealType, setSelectedMealType] = useState<string>('')
-    console.log('Selected meal type:', selectedMealType)
+    const [searchString, setSearchString] = useState<string>('')
+    const [foods, setFoods] = useState<Food[]>([])
+    const debouncedSearchString = useDebounce(searchString, 800)
+    console.log('debouncedSearchString', debouncedSearchString)
+    console.log('foods', foods)
+    useEffect(() => {
+        let url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${debouncedSearchString}&search_simple=1&action=process&json=1`
+        
+        if (debouncedSearchString.trim() === '') {
+            setFoods([])
+            return
+        }
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+              const products = (data.products || [])
+                .filter((product: any) => !!product.nutriments)
+                .map((product: any) => ({
+                  id: product.id,
+                  name: product.product_name,
+                  calories: product.nutriments['energy-kcal_100g'] || 0,
+                  carbohydrates: product.nutriments['carbohydrates_100g'] || 0,
+                  proteins: product.nutriments['proteins_100g'] || 0,
+                  fats: product.nutriments['fat_100g'] || 0,
+                  imageUrl: product.image_url || '',
+                }))
+              setFoods(products)
+            })
+            .catch((error) => {
+                console.error('Error fetching food data:', error)
+            })
+    }, [debouncedSearchString])
+
     return (
         <View style={{ flex: 1, padding: 16 }}>
             <Text style={styles.title}>Type de repas</Text>
@@ -19,13 +54,13 @@ const AddFoodScreen = () => {
                     onPress={() => setSelectedMealType(mealType.id)}
                     style={[
                         styles.mealTypeButton,
-                        isSelected && styles.mealTypeButtonSelected,
+                        isSelected ? styles.mealTypeButtonSelected : null,
                     ]}
                     >
                     <Text
                         style={[
                         styles.mealTypeText,
-                        isSelected && styles.mealTypeTextSelected,
+                        isSelected ? styles.mealTypeTextSelected : null,
                         ]}
                     >
                         {mealType.name}
@@ -33,6 +68,23 @@ const AddFoodScreen = () => {
                     </Pressable>
                 )
                 })}
+            </View>
+            <View>
+                <Text style={styles.title}>Rechercher un aliment</Text>
+                <View style={styles.searchContainer}>
+                    <View style={styles.searchRow}>
+                        <Ionicons name="search" size={20} color="#8C8C8C" />
+                        <TextInput
+                            style={styles.searchInput}
+                            placeholder="Rechercher un produit..."
+                            value={searchString}
+                            onChangeText={setSearchString}
+                        />
+                    </View>
+                    <Pressable onPress={() => setSearchString('')} style={styles.cameraButton}>
+                        <Ionicons name="scan" size={20} color="white" />
+                    </Pressable>
+                </View>
             </View>
         </View>
     )
@@ -52,7 +104,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     marginBottom: 20,
     gap: 8,
-    flex: 1,
   },
   mealTypeButton: {
     paddingVertical: 14,
@@ -75,5 +126,32 @@ const styles = StyleSheet.create({
   mealTypeTextSelected: {
     color: '#FFFFFF',
   },
-
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 40,
+  },
+  searchRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#E0E0E0',
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    width: '90%',
+  },
+  searchInput: {
+    flex: 1,
+    paddingVertical: 12,
+    marginLeft: 8,
+  },
+  cameraButton: {
+    marginLeft: 8,
+    padding: 8,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#4CAF50',
+    backgroundColor: '#4CAF50',
+    width: '10%',
+  },
 })
